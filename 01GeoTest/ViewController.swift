f7a8d3759d37ed6d6a83346e1da77d767c20e77c
L2H <L2H@l2hui-macbook-pro.local>
2017-10-12 07:05:00 +0900

    Initial Commit

---
+++ 01GeoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  01GeoTest
+//
+//  Created by L2H on 2017. 10. 12..
+//  Copyright © 2017년 L2H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
